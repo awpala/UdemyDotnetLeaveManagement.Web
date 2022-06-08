0d54a52e686582635f7f3d9ac1dc6b0723e373ad
awpala <adam.w.pala@gmail.com>
2022-06-08 11:41:52 -0500

    Initialize LeaveType view model and Automapper config

---
+++ Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UdemyDotnetLeaveManagement.Web.Data;
+using UdemyDotnetLeaveManagement.Web.Models;
+
+namespace UdemyDotnetLeaveManagement.Web.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ Models/LeaveTypeVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyDotnetLeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Default Number of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
